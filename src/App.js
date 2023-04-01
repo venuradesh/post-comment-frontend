bf8da71eb50d89b80320286da74d6f91e410dfe5
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2023-04-01 10:50:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <Container>
+      <div className="container">app</div>
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div``;
